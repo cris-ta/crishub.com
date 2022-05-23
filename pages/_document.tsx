43d44e55dc3eeb8de645d4372fee58c829988728
Cris Ta <canh.tc98@gmail.com>
2022-05-23 11:23:35 +0700

    feat: add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,7 +15,7 @@ const Doc = () => (
       <meta name="monetization" content="$ilp.uphold.com/nZ4DF39aHkrm" />
       <link
         rel="icon"
-        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🥐</text></svg>"
+        href='/assets/favicon.ico'
       />
     </Head>
     <body>
